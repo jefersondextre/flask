8ecb7a72620d9cc4c1b9f5bf6cf851e578a21de4
jadex_Desktop <jadexb04@gmail.com>
2022-06-18 18:48:33 -0500

    Video2.4.1 CREACION DE URLs  primer uso de redirect y url_for

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,redirect
+from flask import Flask,redirect,url_for
 
 app=Flask(__name__)
 
@@ -7,34 +7,43 @@ def index():
   return """
         <h1 style="font-size:32px; color:steelblue;">Inicio</h1>
         """
+        
 @app.route("/contacto")
 def contact():
   return """
         <h1 style="font-size:32px; color:green;">Contact</h1>
         """
         
-@app.route("/Nosotros")
+@app.route("/nosotros/")
 def about():
   return """
           <h1 style="font-size:32px; color:green;">Nosotros</h1>
         """
+# URL_FOR 
+@app.route("/ejemplo")
+def example():
+  # print(url_for("dashboard",name="John",age="27")) # A la url_for() le estamos pasando 
+  # como parametro el nombre de una 
+  # funcion que esta siendo administrada por 
+  # una URL  
+  return redirect(url_for("about"))
 
-# Este metodo siempre espera recibir la url con el nombre para eso puedes colocar otro decorador
+
+# Este metodo siempre espera recibir la url con el nombre para eso 
+# puedes colocar otro decorador
 @app.route("/login/<name>/")
 @app.route("/login/")
 def login(name=None):
   print(name)
   if name is not None:
-    return redirect(f"/dashboard/{name}")
+    return redirect(f"/dashboard/{name}/")
   else:
     return f'<h1 style="font-size:32px; color:green;">Hola Desconocido </h1> <br> <p>Por favor igresa tu nombre en la URL</p>'
 
 
-@app.route("/dashboard/<name>/")
-@app.route("/dashboard/")
+@app.route("/profile/<name>/")
+@app.route("/profile/")
 def dashboard(name=None):
-  # print("In dashboard")
   if name is not None:
     return f"<h1>Bienvenido {name} al dashboard</h1>"
-  # return redirect("/login/")
-  return redirect("https://google.com")
+  return redirect("/login/")
