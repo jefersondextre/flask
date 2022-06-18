32632cd10aa01d1a25330968eebc9dc65cb7470b
jadex_Desktop <jadexb04@gmail.com>
2022-06-18 09:20:58 -0500

    VIDEO 2.2 - Manejando URLs Dinámicas, parámetros por defecto y tipos de datos enviados como parámetros

--- app.py
+++ app.py
@@ -21,3 +21,16 @@ def about():
   return """
           <h1 style="font-size:32px; color:green;">Nosotros</h1>
         """
+
+# Este metodo siempre espera recibir la url con el nombre para eso puedes colocar otro decorador
+@app.route("/Saludo/<name>/<int:sueldo>/")
+@app.route("/Saludo/<name>/")
+@app.route("/Saludo/")
+def greeting(name=None,sueldo=None):
+  print(name)
+  if name is not None:
+    if sueldo is not None:
+      return f'<h1 style="font-size:32px; color:green;">Hola {name}, Tu sueldo es: {sueldo*2} </h1>'
+    return f'<h1 style="font-size:32px; color:green;">Hola {name} </h1>'
+  else:
+    return f'<h1 style="font-size:32px; color:green;">Hola Desconocido </h1>'
