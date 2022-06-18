0ce53116c4a27b0c9159d1a01eec356754b93d1f
jadex_Desktop <jadexb04@gmail.com>
2022-06-18 08:38:52 -0500

    Video2.1 Generando nuestras URLs basicas

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask
+
+app=Flask(__name__)
+
+# if __name__=='__main__':
+  # ya no necesitamos esta condivion ya que ahora usamos a el propio flask para levantar nuestro servidor.
+  
+@app.route("/")
+def index():
+  return """
+        <h1 style="font-size:32px; color:steelblue;">Inicio</h1>
+        """
+@app.route("/contacto")
+def contact():
+  return """
+        <h1 style="font-size:32px; color:green;">Contact</h1>
+        """
+        
+@app.route("/Nosotros")
+def about():
+  return """
+          <h1 style="font-size:32px; color:green;">Nosotros</h1>
+        """
